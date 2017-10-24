b775d4ad69da6e51d2cc90546245126583d06a61
kaisjessa <kais.jessa@ucc.on.ca>
2017-10-24 19:27:20 -0400

    test

---
+++ Fraction.java
@@ -0,0 +1,23 @@
+public class Fraction {
+  /* When we think of an object we ask ourselves what
+  the attributes and behaviours
+  */
+
+  //Fields
+  int num; //stores numerators
+  int den; //stores denominators
+
+  //Constructors
+  public void multiply(int a) {
+    num = num * a;
+  }
+
+  public void multiplyFraction(Fraction other) {
+    num = num*other.num;
+    den = den*other.den;
+  }
+
+  //Methods
+
+
+}
---
+++ FractionRunner.java
@@ -0,0 +1,23 @@
+public class FractionRunner {
+  public static void main(String[] args) {
+
+    //Reference Type = Object Type
+    Fraction f1 = new Fraction();
+    Fraction f2 = new Fraction();
+
+    f1.num = 1;
+    f1.den = 2;
+
+    f2.num = 3;
+    f2.den = 4;
+
+    System.out.println(f1.num+"/"+f1.den);
+    f1.multiply(5);
+    System.out.println(f1.num+"/"+f1.den);
+    f2.multiply(100);
+    System.out.println(f2.num+"/"+f2.den);
+    f1.multiplyFraction(f2);
+    System.out.println(f1.num+"/"+f1.den);
+    System.out.println(f2.num+"/"+f2.den);
+  }
+}
